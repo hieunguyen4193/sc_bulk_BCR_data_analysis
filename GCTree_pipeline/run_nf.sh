bac9e2d06b82dfc6a1c46b4e7dd1e560010ee62a
hieunguyen4193 <work@hieunguyen.de>
2024-11-07 14:10:20 +0100

    update

--- GCTree_pipeline/run_nf.sh
+++ GCTree_pipeline/run_nf.sh
@@ -5,5 +5,4 @@ deduplicate_src=$3;
 nextflow run GCtree_pipeline_input_SampleSheet.nf \
 --samplesheet ${path_to_samplesheet} \
 --deduplicate_src ${deduplicate_src} \
---output ${output}
--resume  
+--output ${output} -resume  
