7a2750e00a61f1dfadf8dbab627f3cd16d9ab4e3
hieunguyen4193 <work@hieunguyen.de>
2024-11-07 15:00:53 +0100

    update

--- GCTree_pipeline/run_nf.sh
+++ GCTree_pipeline/run_nf.sh
@@ -11,7 +11,7 @@ nextflow run GCtree_pipeline_input_SampleSheet.nf \
 # for mouseID in m1 m2 m3;do for inputCase in all without_colon_sample;do \
 # bash run_nf.sh \
 # /home/hieu/src/sc_bulk_BCR_data_analysis/SampleSheet_GCTree_nextflow_240826_BSimons/SampleSheet_GCTree_240826_BSimons_${mouseID}_${inputCase}.nextflow.csv \
-# /home/hieu/outdir/ \
+# /home/hieu/outdir/${mouseID}_${inputCase} \
 # /home/hieu/src/sc_bulk_BCR_data_analysis/GCTree_pipeline/deduplicated.py \
 # /home/hieu/src/sc_bulk_BCR_data_analysis/GCTree_pipeline/work_${mouseID}_${inputCase};
 # rm -rf /home/hieu/src/sc_bulk_BCR_data_analysis/GCTree_pipeline/work_${mouseID}_${inputCase};
