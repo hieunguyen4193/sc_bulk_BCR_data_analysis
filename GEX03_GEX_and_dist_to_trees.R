04868a6932a66b332d111816acf06e51c5799b12
hieunguyen4193 <work@hieunguyen.de>
2024-12-03 11:02:56 +0100

    update

---
+++ GEX03_GEX_and_dist_to_trees.R
@@ -0,0 +1,30 @@
+gc()
+rm(list = ls())
+
+#####----------------------------------------------------------------------#####
+##### packages
+#####----------------------------------------------------------------------#####
+path.to.main.src <- "/media/hieunguyen/HNSD01/src/sc_bulk_BCR_data_analysis"
+source(file.path(path.to.main.src, "VDJ_helper_functions.R"))
+
+scrna_pipeline_src <- "/media/hieunguyen/HNSD01/src/src_pipeline/scRNA_GEX_pipeline/processes_src"
+source(file.path(scrna_pipeline_src, "import_libraries.R"))
+source(file.path(scrna_pipeline_src, "helper_functions.R"))
+source(file.path(scrna_pipeline_src, "s8_integration_and_clustering.R"))
+
+#####---------------------------------------------------------------------------#####
+##### INPUT ARGS
+#####---------------------------------------------------------------------------#####
+path.to.storage <- "/media/hieunguyen/HNSD01/storage/all_BSimons_datasets"
+path.to.project.src <- "/media/hieunguyen/HNSD01/src/sc_bulk_BCR_data_analysis"
+source(file.path(path.to.main.src, "GEX_path_to_seurat_obj.addedClone.R"))
+
+outdir <- "/media/hieunguyen/GSHD_HN01/outdir/sc_bulk_BCR_data_analysis_v0.1"
+input.dataset <- "240805_BSimons"
+path.to.03.output <- file.path(outdir, "GEX_output", "03_output", input.dataset)
+dir.create(path.to.03.output, showWarnings = FALSE, recursive = TRUE)
+
+print(sprintf("Working on dataset %s", input.dataset))
+s.obj <- readRDS(path.to.all.s.obj[[input.dataset]])
+DefaultAssay(s.obj) <- "RNA"
+  
