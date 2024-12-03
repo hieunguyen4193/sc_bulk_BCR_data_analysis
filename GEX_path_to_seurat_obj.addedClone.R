394e975914fd7da50f4eb5e357d92d4adecb48e5
hieunguyen4193 <work@hieunguyen.de>
2024-12-04 00:31:16 +0100

    update

--- GEX_path_to_seurat_obj.addedClone.R
+++ GEX_path_to_seurat_obj.addedClone.R
@@ -4,7 +4,7 @@ path.to.all.s.obj <- list(
   `1st_dataset` = file.path(path.to.00.output, "1st_dataset", "1st_dataset_removed_7_9_removed_16_removed_9.with_reInt.res1.addedCloneInfo.rds"),
   `2nd_dataset` = file.path(path.to.00.output, "2nd_dataset", "2nd_dataset_removed_5_6.without_reInt.res1.addedCloneInfo.rds"),
   `240805_BSimons` = file.path(path.to.00.output, "240805_BSimons", "240805_BSimons.output.s8.addedCloneInfo.rds"),
-  `241104_BSimons` = file.path(path.to.00.output, "241002_BSimons", "GEX_sample_PP3_seurat_object.addedCloneInfo.rds"),
-  `241002_BSimons` = file.path(path.to.00.output, "241104_BSimons", "GEX_sample_PP7_seurat_object.addedCloneInfo.rds")
+  `241002_BSimons` = file.path(path.to.00.output, "241002_BSimons", "GEX_sample_PP3_seurat_object.addedCloneInfo.rds"),
+  `241104_BSimons` = file.path(path.to.00.output, "241104_BSimons", "GEX_sample_PP7_seurat_object.addedCloneInfo.rds")
 )
 
