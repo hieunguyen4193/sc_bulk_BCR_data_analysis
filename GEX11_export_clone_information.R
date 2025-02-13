fd26ddcd429052c2bcf2a2a1bf0ac3c6839a2610
hieunguyen4193 <work@hieunguyen.de>
2025-02-13 07:51:18 +0100

    update

--- GEX11_export_clone_information.R
+++ GEX11_export_clone_information.R
@@ -30,25 +30,53 @@ outdir <- "/media/hieunguyen/GSHD_HN01/outdir/sc_bulk_BCR_data_analysis_v0.1"
 
 path.to.11.output <- file.path(outdir, "GEX_output", "11_output")
 dir.create(path.to.11.output, showWarnings = FALSE, recursive = TRUE)
-
+rerun <- TRUE
 clonedf <- list()
+sc.dataset.with.hto <- c("240805_BSimons",
+                         "241104_BSimons",
+                         "240805_BSimons_filterHT",
+                         "240805_BSimons_filterHT_cluster_renamed",
+                         "241002_BSimons",
+                         "240805_BSimons_filterHT_cluster",
+                         "241002_241104_BSimons")
 for (input.dataset in names(path.to.all.s.obj)){
-  if (file.exists(file.path(path.to.11.output, sprintf("%s.csv", input.dataset))) == FALSE){
+  if (file.exists(file.path(path.to.11.output, sprintf("%s.csv", input.dataset))) == FALSE | rerun == TRUE){
     s.obj <- readRDS(path.to.all.s.obj[[input.dataset]])
-    meta.data <- s.obj@meta.data %>% subset(select = c(
-      CTaa,
-      CTstrict,
-      V.gene, 
-      J.gene, 
-      aaSeqCDR3, 
-      nSeqCDR3, 
-      VJseq.combi,
-      VJ.combi,
-      VJ.len.combi,        
-      VJcombi_CDR3_0.85 
-    )) %>% 
-      subset(is.na(VJcombi_CDR3_0.85) == FALSE) %>% 
-      rownames_to_column("barcode")
+    if (input.dataset %in% sc.dataset.with.hto){
+      meta.data <- s.obj@meta.data %>% subset(select = c(
+        CTaa,
+        CTstrict,
+        V.gene, 
+        J.gene, 
+        aaSeqCDR3, 
+        nSeqCDR3, 
+        VJseq.combi,
+        VJ.combi,
+        VJ.len.combi,        
+        VJcombi_CDR3_0.85,
+        HTO_classification,
+        HTO_classification.global
+      )) %>% 
+        subset(is.na(VJcombi_CDR3_0.85) == FALSE) %>% 
+        rownames_to_column("barcode")
+    } else {
+      meta.data <- s.obj@meta.data %>% subset(select = c(
+        CTaa,
+        CTstrict,
+        V.gene, 
+        J.gene, 
+        aaSeqCDR3, 
+        nSeqCDR3, 
+        VJseq.combi,
+        VJ.combi,
+        VJ.len.combi,        
+        VJcombi_CDR3_0.85
+      )) %>% 
+        subset(is.na(VJcombi_CDR3_0.85) == FALSE) %>% 
+        rownames_to_column("barcode")
+    }
+
+    
     write.csv(meta.data, file.path(path.to.11.output, sprintf("%s.csv", input.dataset)))
     clonedf[[input.dataset]] <- meta.data
   } else {
@@ -86,21 +114,21 @@ if (file.exists(file.path(path.to.11.output, "check_SPEC_IGH_clone_definition.xl
 }
 
 
-input.dataset <- "1st_dataset"
-df <- clonedf[[input.dataset]]
-dfcount <- table(df$VJcombi_CDR3_0.85) %>% sort(decreasing = TRUE)
-df.unique <- data.frame()
-for (x in names(dfcount)){
-  tmp.ctstrict <- subset(df, df$VJcombi_CDR3_0.85 == x)$CTstrict %>% unique()
-  tmpdf <- data.frame(clone = rep(x, length(tmp.ctstrict)))
-  tmpdf$clone.CTStrict <- tmp.ctstrict 
-  tmpdf <- tmpdf %>% rowwise() %>%
-    mutate(num.cell = subset(df, df$CTstrict == clone.CTStrict & df$VJcombi_CDR3_0.85 == x)$barcode %>% length()) 
-  tmpdf <- tmpdf %>% rowwise() %>% mutate(pct.cell = num.cell/sum(tmpdf$num.cell))
-  df.unique <- rbind(df.unique, tmpdf)
-}
-
-top100.clones <- head(dfcount, 20) %>% names()
-df.unique %>% subset(clone %in% top100.clones) %>% ggplot(aes(x = clone, y = pct.cell, color = "black")) + 
-  geom_bar(stat = "identity") +
-  theme(legend.position = "none", axis.text.x = element_text(angle = 90) ) 
+# input.dataset <- "1st_dataset"
+# df <- clonedf[[input.dataset]]
+# dfcount <- table(df$VJcombi_CDR3_0.85) %>% sort(decreasing = TRUE)
+# df.unique <- data.frame()
+# for (x in names(dfcount)){
+#   tmp.ctstrict <- subset(df, df$VJcombi_CDR3_0.85 == x)$CTstrict %>% unique()
+#   tmpdf <- data.frame(clone = rep(x, length(tmp.ctstrict)))
+#   tmpdf$clone.CTStrict <- tmp.ctstrict 
+#   tmpdf <- tmpdf %>% rowwise() %>%
+#     mutate(num.cell = subset(df, df$CTstrict == clone.CTStrict & df$VJcombi_CDR3_0.85 == x)$barcode %>% length()) 
+#   tmpdf <- tmpdf %>% rowwise() %>% mutate(pct.cell = num.cell/sum(tmpdf$num.cell))
+#   df.unique <- rbind(df.unique, tmpdf)
+# }
+# 
+# top100.clones <- head(dfcount, 20) %>% names()
+# df.unique %>% subset(clone %in% top100.clones) %>% ggplot(aes(x = clone, y = pct.cell, color = "black")) + 
+#   geom_bar(stat = "identity") +
+#   theme(legend.position = "none", axis.text.x = element_text(angle = 90) ) 
